41241180c7e99fb28761819c4e2767261e18d1de
brightkingpin <luvandabright781@gmail.com>
2023-03-17 18:53:35 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  // My scuberGreetingForFeer code
+  let message;
+  if (feet <= 400) {
+    message = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    message = 'No can do.';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  // My ternary code
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  // My Switch code
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+
 }
